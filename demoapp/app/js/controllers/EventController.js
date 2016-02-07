6dfcf8731b90b48ad1dd9a3faa7fd754f3659a5b
Christopher Henderson <christopher.henderson1989@gmail.com>
2016-02-07 20:15:00 +0000

    angualr app from pluralsight

---
+++ demoapp/app/js/controllers/EventController.js
@@ -0,0 +1,47 @@
+'use strict';
+
+eventsApp.controller('EventController',
+  function EventController($scope, eventData, $anchorScroll) {
+
+    $scope.snippet = '<span style="color:red">hi there</span>';
+
+    $scope.boolValue = true;
+
+    $scope.mystyle = {color:'red'};
+
+    $scope.myclass = "blue";
+
+    $scope.buttonDisabled = true;
+
+    $scope.sortorder = 'name';
+
+    eventData.getEvent()
+      .$promise
+      .then(function(event) { $scope.event = event; console.log(event); })
+      .catch(function(response) { console.log(response); });
+
+    /*
+    // Form $http
+    eventData.getEvent()
+      .success(function(event) {
+        $scope.event = event;
+      })
+      .error(function(data, status, headers, config){
+        $log.warn(data, status, headers, config);
+      });
+
+    */
+
+    $scope.upVoteSession = function(session) {
+      session.upVoteCount++;
+    };
+
+    $scope.downVoteSession = function(session) {
+      session.upVoteCount--;
+    }
+
+    $scope.scrollToSession = function() {
+      $anchorScroll();
+    }
+  }
+);
